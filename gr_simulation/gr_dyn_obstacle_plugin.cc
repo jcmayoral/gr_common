3929e61b2d78c32745500301295a655bb9203249
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-03-13 18:20:59 +0100

    clean gr_sim

--- gr_simulation/gr_dyn_obstacle_plugin.cc
+++ gr_simulation/gr_dyn_obstacle_plugin.cc
@@ -21,22 +21,21 @@ namespace gazebo
  
     public: void SetAngVelocity(const double &_vel){
         ang_velocity = _vel;
-	this->link->SetAngularVel(ignition::math::Vector3<double>(0.0,0.0,ang_velocity));		 
+        this->link->SetAngularVel(ignition::math::Vector3<double>(0.0,0.0,ang_velocity));		 
     }
     public: void SetLinearVelocityX(const double &_vel){
         lin_velx = _vel;
-	this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
+        this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
     }
     public: void SetLinearVelocityY(const double &_vel){
         lin_vely = _vel;
-	this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
+        this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
     }
 
     private: void OnMsg(ConstVector3dPtr &_msg){
-printf("%s","working callback");
-	this->SetLinearVelocityX(_msg->x());
-	this->SetLinearVelocityY(_msg->y());
-	this->SetAngVelocity(_msg->z());
+        this->SetLinearVelocityX(_msg->x());
+        this->SetLinearVelocityY(_msg->y());
+        this->SetAngVelocity(_msg->z());
     }
 
     /// \brief The load function is called by Gazebo when the plugin is
@@ -44,77 +43,49 @@ printf("%s","working callback");
     /// \param[in] _model A pointer to the model that this plugin is
     /// attached to.
     /// \param[in] _sdf A pointer to the plugin's SDF element.
-    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
-    {
-    
-
-
-      // Just output a message for now
-      std::cerr << "\nThe velodyne plugin is attach to model[" <<
+    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
+        // Just output a message for now
+        std::cerr << "\nThe velodyne plugin is attach to model[" <<
         _model->GetName() << "]\n";
 
 
-      if (_sdf->HasElement("ang_velocity")){
-	      ang_velocity = _sdf->Get<double>("ang_velocity");
-      }
-      if (_sdf->HasElement("lin_velx")){
-	      lin_velx = _sdf->Get<double>("lin_velx");
-      }
-      if (_sdf->HasElement("lin_vely")){
-	      lin_vely = _sdf->Get<double>("lin_vely");
-      }
-       // Safety check
-       //if (_model->GetJointCount() == 0){
-	//std::cerr << "Invalid joint count, Velodyne plugin not loaded\n";
-    	//return;
-      	// }
- 
-       // Store the model pointer for convenience.
-       this->model = _model;
-        // Get the first joint. We are making an assumption about the model
-	// having one joint that is the rotational joint.
-	//this->joint = _model->GetJoints()[0];
-	//printf("%b" % this->model->GetGravityMode());
-	this->model->SetGravityMode(false);
-	//printf ("%d" % len(this->model->GetLinks()));
-	this->link = _model->GetLinks()[0];
-
-	// Setup a P-controller, with a gain of 0.1.
-	//this->pid = common::PID(0.1, 0, 0);
-
-	// Apply the P-controller to the joint.
-	this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
-	this->link->SetAngularVel(ignition::math::Vector3<double>(0.0,0.0,ang_velocity));
-	//this->model->GetJointController()->SetVelocityPID(this->joint->GetScopedName(), this->pid);
-
-	// Set the joint's target velocity. This target velocity is just
-	// // for demonstration purposes.
-	//this->model->GetJointController()->SetVelocityTarget(this->joint->GetScopedName(), 10.0);
-    // Create the node
-    this->node = transport::NodePtr(new transport::Node());
-    this->node->Init(this->model->GetWorld()->Name());
-    // Create a topic name
-    std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
-    // Subscribe to the topic, and register a callback
-    this->sub = this->node->Subscribe(topicName,&GRDynObstaclePlugin::OnMsg, this);
+        if (_sdf->HasElement("ang_velocity")){
+            ang_velocity = _sdf->Get<double>("ang_velocity");
+        }
+        if (_sdf->HasElement("lin_velx")){
+            lin_velx = _sdf->Get<double>("lin_velx");
+        }
+        if (_sdf->HasElement("lin_vely")){
+            lin_vely = _sdf->Get<double>("lin_vely");
+        }
+        
+        this->model = _model;
+        this->model->SetGravityMode(false);
+        this->link = _model->GetLinks()[0];
+        this->link->SetLinearVel(ignition::math::Vector3<double>(lin_velx,lin_vely,0.0));
+        this->link->SetAngularVel(ignition::math::Vector3<double>(0.0,0.0,ang_velocity));
+        this->node = transport::NodePtr(new transport::Node());
+        this->node->Init(this->model->GetWorld()->Name());
+        std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
+        this->sub = this->node->Subscribe(topicName,&GRDynObstaclePlugin::OnMsg, this);
     }
     /// \brief Pointer to the model.
-private: physics::ModelPtr model;
+    private: physics::ModelPtr model;
 
-/// \brief Pointer to the joint.
-private: physics::JointPtr joint;
+    /// \brief Pointer to the joint.
+    private: physics::JointPtr joint;
 
-private: physics::LinkPtr link;
+    private: physics::LinkPtr link;
 
-/// \brief A PID controller for the joint.
-private: common::PID pid;
+    /// \brief A PID controller for the joint.
+    private: common::PID pid;
 
 
-	 /// \brief A node used for transport
-private: transport::NodePtr node;
+        /// \brief A node used for transport
+    private: transport::NodePtr node;
 
-/// \brief A subscriber to a named topic.
-private: transport::SubscriberPtr sub;
+    /// \brief A subscriber to a named topic.
+    private: transport::SubscriberPtr sub;
   };
 
   // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
