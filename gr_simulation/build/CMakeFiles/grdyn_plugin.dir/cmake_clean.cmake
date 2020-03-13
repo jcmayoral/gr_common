1da770314b7684d2a04c83b9b2d7f28a5fe7788b
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-03-13 11:14:12 +0100

    first attempt to simulator(gazebo plugins tutorials)

---
+++ gr_simulation/build/CMakeFiles/grdyn_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grdyn_plugin.dir/gr_dyn_obstacle_plugin.cc.o"
+  "libgrdyn_plugin.pdb"
+  "libgrdyn_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grdyn_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
