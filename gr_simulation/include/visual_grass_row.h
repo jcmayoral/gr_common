e66a81d00ad7696f35ec962fa7de67220f4ad269
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-03-20 16:07:32 +0100

    visual plugin working :D

---
+++ gr_simulation/include/visual_grass_row.h
@@ -0,0 +1,74 @@
+#ifndef _GAZEBO_VISUAL_GRASS_ROW_HH_
+#define _GAZEBO_VISUAL_GRASS_ROW_HH_
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <ignition/math/Vector3.hh>
+#include <gazebo/rendering/Visual.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <thread>
+#include "grasscutter.pb.h"
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+
+#include <std_msgs/Bool.h>
+
+namespace gazebo
+{
+  typedef const boost::shared_ptr<const gr_simulation_msgs::msgs::GrassCutterRequest> GrassCutterRequestPtr;
+  /// \brief A plugin to control a GR Dynamic Obstacle sensor.
+  class VisualGrassRow : public VisualPlugin
+  {
+    /// \brief Constructor
+    public: 
+        VisualGrassRow();
+        ~VisualGrassRow();
+
+        /// \brief The load function is called by Gazebo when the plugin is
+        /// inserted into simulation
+        /// \param[in] _model A pointer to the model that this plugin is
+        /// attached to.
+        /// \param[in] _sdf A pointer to the plugin's SDF element.
+        //virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+        virtual void Load(rendering::VisualPtr _parent, sdf::ElementPtr _sdf);
+        //virtual void Init();
+        void ros_cb(const std_msgs::BoolConstPtr &_msg);
+    private: 
+        void OnEvent(bool state);
+        void OnRequest(GrassCutterRequestPtr &event);
+        void QueueThread();
+
+        /// \brief Pointer to the model.
+        rendering::VisualPtr model;
+        std::string model_id;
+        //physics::LinkPtr link;
+        
+        ignition::math::Pose3<double> current_pose;
+        double ang_velocity = 0;
+        double lin_velx = 0;
+        double lin_vely = 0;
+        sdf::ElementPtr visualElem;
+
+        int access_counter;
+
+        bool is_cut;
+        /// \brief A node used for transport
+        transport::NodePtr node;
+        transport::SubscriberPtr sub;
+
+        //ROS
+        ros::NodeHandlePtr nh;
+        /// \brief A ROS subscriber
+        ros::Subscriber rosSub;
+        ros::CallbackQueue rosQueue;
+        std::thread rosQueueThread;
+
+  };
+
+  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
+  GZ_REGISTER_VISUAL_PLUGIN(VisualGrassRow)
+}
+#endif
+
---
+++ gr_simulation/src/visual_grass_row.cpp
@@ -0,0 +1,96 @@
+#include <visual_grass_row.h>
+#include <gtest/gtest.h>
+
+
+using namespace gazebo;
+
+VisualGrassRow::VisualGrassRow():is_cut(false), access_counter(0){
+    // Initialize ros, if it has not already bee initialized.
+}
+
+VisualGrassRow::~VisualGrassRow(){
+    std::cout << "KILLED?"<<std::endl;
+    this->model.reset();
+}
+
+/*void VisualGrassRow::Init(){
+    std::cout << "Init " << access_counter << std::endl;
+    access_counter++;
+    if (this->rosSub.getTopic().empty()){
+        ROS_INFO("initialize subscriber");
+        this->rosSub = this->nh->subscribe("test", 1, &VisualGrassRow::ros_cb, this);
+        ROS_INFO("subscriber working");
+        ros::spinOnce();
+    }
+    //std::cout << "SUbscribed to topic " << this->sub->GetTopic() << std::endl;
+    //OnEvent();
+
+}*/
+
+//seems Load function runs twice when added manually on Gazebo server
+//TODO review behavior when a world is loaded
+void VisualGrassRow::Load(rendering::VisualPtr _parent, sdf::ElementPtr _sdf){
+    
+    this->model = _parent;
+    std::cout << "Load " << access_counter << std::endl;
+    access_counter++;
+
+    bool use_ros=false;
+
+    if (_sdf->HasElement("use_ros")){
+        use_ros = _sdf->Get<bool>("use_ros");
+    }
+
+    model_id = this->model->Name();//this->model->Name();      
+    std::cout << "Model Name " << model_id << std::endl;
+    auto start_idx = model_id.find("grassrow");
+    auto end_idx = model_id.find("link")-2;
+    model_id = model_id.substr(start_idx, end_idx);
+    std::string topicName = model_id;
+
+    if (!use_ros){
+        this->node = transport::NodePtr(new transport::Node());
+        this->node->Init();//this->model->Name());    
+        this->sub = this->node->Subscribe(topicName,&VisualGrassRow::OnRequest, this);
+    }
+
+    if (use_ros){
+        if (!ros::isInitialized()){
+            int argc = 0;
+            char **argv = NULL;
+            ros::init(argc, argv, "gazebo_client",
+            ros::init_options::NoSigintHandler);
+        }
+        this->nh = boost::make_shared<ros::NodeHandle>();
+        //Subscriber
+        ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Bool>(
+            topicName,1,
+            boost::bind(&VisualGrassRow::ros_cb, this, _1),
+            ros::VoidPtr(), &this->rosQueue);    
+        this->rosSub = this->nh->subscribe(so);
+        // Spin up the queue helper thread.
+        this->rosQueueThread = std::thread(std::bind(&VisualGrassRow::QueueThread, this));
+    }
+}
+
+void VisualGrassRow::ros_cb(const std_msgs::BoolConstPtr &_msg){
+    OnEvent(_msg->data);
+}
+
+void VisualGrassRow::OnRequest(GrassCutterRequestPtr &event){
+    OnEvent(event->cut());
+}
+
+
+void VisualGrassRow::OnEvent(bool state){
+    is_cut = state;
+    this->model->SetVisible(is_cut);
+    //this->model->Update();
+}
+
+void VisualGrassRow::QueueThread(){
+    static const double timeout = 0.01;
+    while (this->nh->ok()){
+        this->rosQueue.callAvailable(ros::WallDuration(timeout));
+    }
+}
