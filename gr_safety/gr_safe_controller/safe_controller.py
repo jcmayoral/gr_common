0f83080f5aa47b16967d5fd16a569f56d8b74e67
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-09-05 11:57:15 +0200

    safe_controller -> penalize cmd_vel

--- gr_safety/gr_safe_controller/safe_controller.py
+++ gr_safety/gr_safe_controller/safe_controller.py
@@ -6,18 +6,22 @@ from std_msgs.msg import Float32
 class SafeController:
     def __init__(self):
         rospy.init_node("safe_controller")
-        self.lin_speed = 0.8
         self.penalization_factor = 0.0
-        self.cmd_pub = rospy.Publisher("/nav_vel", Twist)
+        self.twist = Twist()
+        self.cmd_pub = rospy.Publisher("/safe_nav_vel", Twist,1)
+        #change to message filter?
         rospy.Subscriber("/safety_score",Float32, self.safety_cb)
+        rospy.Subscriber("/nav_vel",Twist, self.vel_cb)
+
         rospy.Timer(rospy.Duration(0.1), self.timer_event)
         rospy.spin()
 
     def timer_event(self,event):
-        twist = Twist()
-        twist.linear.x = self.lin_speed * (1-self.penalization_factor)
-        rospy.loginfo(twist)
-        self.cmd_pub.publish(twist)
+        self.twist.linear.x = self.twist.linear.x * (1-self.penalization_factor)
+        self.cmd_pub.publish(self.twist)
+
+    def vel_cb(self,msg):
+        self.twist = msg
 
     def safety_cb(self,msg):
         self.penalization_factor = msg.data
