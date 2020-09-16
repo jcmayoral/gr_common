5b56844dde0f16927aadde82ae3b41816a629946
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-09-16 10:27:15 +0200

    fixes

--- gr_safety/gr_safe_controller/safe_controller.py
+++ gr_safety/gr_safe_controller/safe_controller.py
@@ -8,12 +8,12 @@ class SafeController:
         rospy.init_node("safe_controller")
         self.penalization_factor = 0.0
         self.twist = Twist()
-        self.cmd_pub = rospy.Publisher("/safe_nav_vel", Twist,1)
+        self.cmd_pub = rospy.Publisher("/safe_nav_vel", Twist,queue_size=1)
         #change to message filter?
         rospy.Subscriber("/safety_score",Float32, self.safety_cb)
         rospy.Subscriber("/nav_vel",Twist, self.vel_cb)
 
-        rospy.Timer(rospy.Duration(0.1), self.timer_event)
+        #rospy.Timer(rospy.Duration(0.05), self.timer_event)
         rospy.spin()
 
     def timer_event(self,event):
@@ -22,6 +22,9 @@ class SafeController:
 
     def vel_cb(self,msg):
         self.twist = msg
+        self.twist.linear.x = self.twist.linear.x * (1-self.penalization_factor)
+        self.cmd_pub.publish(self.twist)
+
 
     def safety_cb(self,msg):
         self.penalization_factor = msg.data
