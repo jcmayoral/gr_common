d27bbbc30612c01ac00d0ff7146bda3d9385d9b7
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-09-23 17:30:55 +0200

    message filters

--- gr_experiments/risk_tools.py
+++ gr_experiments/risk_tools.py
@@ -1,26 +1,52 @@
 from metrics import *
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, Float32
 from safety_msgs.msg import FoundObjectsArray
 from scipy.spatial import distance
 import rospy
+from numpy import cov
+import message_filters
 
 class RiskExtractor:
     def __init__(self):
         rospy.init_node("gr_experiment_tool")
         self.obstacles = []#dict()
+        self.safety_scores = []
         self.run = True
-        self.subscriber = rospy.Subscriber("/pointcloud_lidar_processing/found_object", FoundObjectsArray, self.cb)
+        #self.subscriber = rospy.Subscriber("/pointcloud_lidar_processing/found_object", FoundObjectsArray, self.cb)
         self.subscriber2 = rospy.Subscriber("/test_finished", Empty, self.test_cb)
+        #self.subscriber3 = rospy.Subscriber("/safety_score", Float32, self.cb2)
+
+        sub1 = message_filters.Subscriber("/pointcloud_lidar_processing/found_object", FoundObjectsArray)
+        sub2 = message_filters.Subscriber("/safety_score", Float32)
+        self.ts = message_filters.ApproximateTimeSynchronizer([sub1, sub2], queue_size=10, slop=0.5, allow_headerless=True)
+        self.ts.registerCallback(self.timed_cb)
 
         while not rospy.is_shutdown() and self.run:
             pass
 
-        self.execute()
+        print "OBSTACLES"
+        self.execute(self.obstacles)
+        print "SAFETY SCORES ", self.safety_scores
+        self.execute(self.safety_scores)
+
+        print "COVARIANCE ", cov(self.obstacles, self.safety_scores)
+
+    def timed_cb(self, persons, score):
+        print "timed cb", len(self.obstacles), len(self.safety_scores)
+        #print "error ", len(persons.objects) <1
+        if len(persons.objects) <1:
+            return
+        self.cb(persons)
+        self.cb2(score)
 
     def test_cb(self, flag):
         print "out"
         self.run = False
-        self.subscriber.unregister()
+        #self.subscriber.unregister()
+        self.subscriber3.unregister()
+
+    def cb2(self, score):
+        self.safety_scores.append(score.data)
 
     def cb(self, persons):
         #Note distance on velodyne fixed
@@ -28,8 +54,8 @@ class RiskExtractor:
         for p in persons.objects:
             self.obstacles.append(distance.euclidean((p.pose.position.x, p.pose.position.y, p.pose.position.z), (0,0,0)))
 
-    def execute(self):
-        print "Number of detections " , len(self.obstacles)
-        print "SM1 Metric: " , SM1(self.obstacles)
-        print "SM2 Metric: " , SM2(self.obstacles)
-        print "SM3 Metric: " , SM3(self.obstacles)
+    def execute(self, data):
+        print "Number of detections " , len(data)
+        print "SM1 Metric: " , SM1(data)
+        print "SM2 Metric: " , SM2(data)
+        print "SM3 Metric: " , SM3(data)
