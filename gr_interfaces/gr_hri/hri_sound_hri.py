2533ac65197bc7beca9a633045484b1debda4f9b
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-10-15 17:44:04 +0200

    small fixes

--- gr_interfaces/gr_hri/hri_sound_hri.py
+++ gr_interfaces/gr_hri/hri_sound_hri.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python
 import rospy
 from playsound import playsound
 from std_msgs.msg import Float32
 import os
 import random
+import time
 
 class ROSPlaySound:
     def __init__(self):
         rospy.init_node("ros_play_sound")
         self.levels=["SAFE", "WARNING", "DANGER"]
         self.file_root = "/home/jose/.safe_sounds"
-        rospy.Subscriber("/test_cb", Float32, self.safe_cb)
+        rospy.Subscriber("/safety_score", Float32, self.safe_cb)
         rospy.spin()
 
     def safe_cb(self, msg):
@@ -17,6 +19,7 @@ class ROSPlaySound:
         path = os.path.join(self.file_root, self.levels[index])
         file_ = random.choice(os.listdir(path))
         playsound(os.path.join(path,file_))
+        time.sleep(1)
 
 
 
