57e67462bf4638627af562d2f5cf8225c0e27fb1
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2020-10-16 13:25:50 +0200

    memorize state

--- gr_interfaces/gr_hri/hri_sound_hri.py
+++ gr_interfaces/gr_hri/hri_sound_hri.py
@@ -9,16 +9,32 @@ import time
 class ROSPlaySound:
     def __init__(self):
         rospy.init_node("ros_play_sound")
+        self.current_state = -1
         self.levels=["SAFE", "WARNING", "DANGER"]
         self.file_root = "/home/jose/.safe_sounds"
+        self.ring_start()
         rospy.Subscriber("/safety_score", Float32, self.safe_cb)
         rospy.spin()
 
-    def safe_cb(self, msg):
-        index = int(msg.data/len(self.levels))
-        path = os.path.join(self.file_root, self.levels[index])
+    def ring_start(self):
+        path = os.path.join(self.file_root, "START")
         file_ = random.choice(os.listdir(path))
         playsound(os.path.join(path,file_))
+        self.current_state = 0
+        time.sleep(1)
+
+
+    def safe_cb(self, msg):
+        index = int(msg.data*len(self.levels))
+        print index, msg.data*len(self.levels)
+        if index == self.current_state:
+            rospy.loginfo("avoid state")
+            return
+        path = os.path.join(self.file_root, self.levels[self.current_state], self.levels[index])
+        print path
+        file_ = random.choice(os.listdir(path))
+        playsound(os.path.join(path,file_))
+        self.current_state = index
         time.sleep(1)
 
 
