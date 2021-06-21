591d9cfed518bbea4026d3862590dcf68fc04ec3
jcmayoral <jocamaba1989@gmail.com>
2021-06-21 16:17:04 +0200

    fix movebase to safe speed

--- gr_safety/gr_safe_controller/safe_controller.py
+++ gr_safety/gr_safe_controller/safe_controller.py
@@ -8,10 +8,10 @@ class SafeController:
         rospy.init_node("safe_controller")
         self.penalization_factor = 0.0
         self.twist = Twist()
-        self.cmd_pub = rospy.Publisher("/safe_nav_vel", Twist,queue_size=1)
+        self.cmd_pub = rospy.Publisher("/nav_vel", Twist,queue_size=1)
         #change to message filter?
         rospy.Subscriber("/safety_score",Float32, self.safety_cb)
-        rospy.Subscriber("/nav_vel",Twist, self.vel_cb)
+        rospy.Subscriber("/movebase_vel",Twist, self.vel_cb)
 
         #rospy.Timer(rospy.Duration(0.05), self.timer_event)
         rospy.spin()
