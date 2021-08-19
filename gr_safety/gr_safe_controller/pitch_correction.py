bab0e3ae4e3d4dfe97bb42420c867ca14329b37b
jcmayoral <jocamaba1989@gmail.com>
2021-08-19 12:02:22 +0200

    get_vel_factor

--- gr_safety/gr_safe_controller/pitch_correction.py
+++ gr_safety/gr_safe_controller/pitch_correction.py
@@ -3,6 +3,7 @@ from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Imu
 import tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 class PitchCorrection:
     def __init__(self, pub_visualizer = False):
@@ -30,32 +31,44 @@ class PitchCorrection:
             self.imu_msg.orientation.w)
         return tf.transformations.euler_from_quaternion(quaternion)
 
+    def get_vel_factor(self, pitch):
+        return np.cos(pitch)
+
 
 
 if __name__ =="__main__":
     plt.ion()
     import numpy as np
     i = 0
-    maxsize = 10
+    maxsize = 25
     dt = np.arange(maxsize)
-    data = np.zeros((maxsize,3))
+    data = np.zeros((maxsize,4))
 
     rospy.init_node("imu_experiments")
     pitch_correction = PitchCorrection(pub_visualizer=True)
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    fig.suptitle('Vertically stacked subplots')
+
+
     while not rospy.is_shutdown():
-        ax = plt.axes()
+        #ax = plt.axes()
+        ax2.set_ylim([-0.1,1.1])
 
         pitch_correction.publish_fb()
         if i >= maxsize:
             i=0
-        data[i]  = pitch_correction.get_euler_angles()
+        data[i,:3]  = pitch_correction.get_euler_angles()
+        data[i,3] = pitch_correction.get_vel_factor(data[i,1])
         i = i+1
-        ax.plot(dt, data[:,0])
-        ax.plot(dt, data[:,1])
-        ax.plot(dt, data[:,2])
+        ax1.plot(dt, data[:,0])
+        ax1.plot(dt, data[:,1])
+        ax1.plot(dt, data[:,2])
+        ax2.plot(dt, data[:,3])
 
         plt.draw()
         plt.pause(0.1)
-        plt.cla()
+        ax1.cla()
+        ax2.cla()
         rospy.sleep(0.1)
         #plt.clf()
