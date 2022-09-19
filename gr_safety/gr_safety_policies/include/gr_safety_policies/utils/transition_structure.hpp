220bc955d6fce4a711d2b65e3cd5608cdbf24572
Jose <jocamaba1989@gmail.com>
2022-09-19 13:06:21 -0500

    clear

--- gr_safety/gr_safety_policies/include/gr_safety_policies/utils/transition_structure.hpp
+++ gr_safety/gr_safety_policies/include/gr_safety_policies/utils/transition_structure.hpp
@@ -9,46 +9,4 @@ namespace gr_safety_policies{
 
     typedef std::map<std::string, TransitionInfo> TTransition;
     typedef std::map<std::string, TTransition> TransitionArray;
-
-    class Transition{
-        std::string name_;
-        bool negate_;
-        
-        public:
-            Transition():
-                name_{"default"}, negate_{false}
-            {
-
-            }
-
-            std::string getName(){
-                return name_;
-            }
-
-            bool isNegated(){
-                return negate_;
-            }
-
-            void setName(std::string name){
-                name_ = name;
-            }
-
-
-    };
-
-
-    /*
-    constexpr T& operator[](std::string name){
-        return a[z * Y * X + y * X + x];
-    }
-    */
-        
-
-    struct TransitionsManager
-    {
-        public:
-            std::string name;
-            std::map<std::string,Transition*> transitions;
-    };
-
 }
