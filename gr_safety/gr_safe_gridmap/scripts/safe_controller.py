2f2aa9f7088cf8b9041deb6576e3e6c0ee7c3475
Jose <jocamaba1989@gmail.com>
2022-09-27 15:07:35 -0500

    0.26.2

--- gr_safety/gr_safe_gridmap/scripts/safe_controller.py
+++ gr_safety/gr_safe_gridmap/scripts/safe_controller.py
@@ -3,11 +3,14 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32
 from pitch_correction import PitchCorrection
+from std_srvs.srv import SetBool
 
 class SafeController(PitchCorrection):
     def __init__(self):
         rospy.init_node("safe_controller")
         PitchCorrection.__init__(self)
+        self.limit_speed = False
+        self.s = rospy.Service('/limit_safe_speed', SetBool, self.safe_speed_cb)
 
         self.penalization_factor = 0.0
         self.twist = Twist()
@@ -19,16 +22,24 @@ class SafeController(PitchCorrection):
 
         #rospy.Timer(rospy.Duration(0.05), self.timer_event)
         rospy.spin()
+    
+    def safe_speed_cb(self,req):
+        if req.data:
+            self.limit_speed = True
+        return SetBoolResponse(sucess=True)
 
     def timer_event(self,event):
         self.twist.linear.x = self.twist.linear.x * (1-self.penalization_factor)
         self.cmd_pub.publish(self.twist)
 
     def vel_cb(self,msg):
-        print (self.get_vel_factor(), self.penalization_factor)
+        #print (self.get_vel_factor(), self.penalization_factor)
         self.publish_fb()
         self.twist = msg
         self.twist.linear.x = self.twist.linear.x * self.get_vel_factor() * (1-self.penalization_factor)
+
+        if self.limit_speed:
+            self.limit_speed = min(self.safe_speed, self.limit_speed)
         self.cmd_pub.publish(self.twist)
 
     def safety_cb(self,msg):
